0434beabf6c6dd16867a86aedaf29e91e94b1670
Joshua Thomas <joshua.thomas@gmail.com>
2021-03-27 15:18:03 -0700

    parsing fields and types properly

--- querybuilder.go
+++ querybuilder.go
@@ -11,7 +11,9 @@ type QueryBuilder struct {
 }
 
 func NewQueryBuilder(schema bson.M) *QueryBuilder {
-	qb := QueryBuilder{}
+	qb := QueryBuilder{
+		fieldTypes: map[string]string{},
+	}
 
 	if schema != nil {
 		qb.discoverFields(schema)
@@ -20,18 +22,43 @@ func NewQueryBuilder(schema bson.M) *QueryBuilder {
 	return &qb
 }
 
-func (qb QueryBuilder) discoverFields(schema bson.M) error {
+func (qb QueryBuilder) discoverFields(schema bson.M) {
+	// ensure fieldTypes is set
+	if qb.fieldTypes == nil {
+		qb.fieldTypes = map[string]string{}
+	}
+
 	// bsonType, required, properties at top level
 	// looking for properties field, specifically
 	if properties, ok := schema["properties"]; ok {
 		properties := properties.(bson.M)
-		for key, value := range properties {
-			value := value.(bson.M)
+		qb.iterateProperties("", properties)
+	}
+}
+
+func (qb QueryBuilder) iterateProperties(parentPrefix string, properties bson.M) {
+	// iterate each field within properties
+	for field, value := range properties {
+		switch value := value.(type) {
+		case bson.M:
+			// retrieve the type of the field
 			if bsonType, ok := value["bsonType"]; ok {
-				fmt.Println(key, bsonType)
+				bsonType := bsonType.(string)
+				if bsonType != "object" {
+					qb.fieldTypes[fmt.Sprintf("%s%s", parentPrefix, field)] = bsonType
+					// fmt.Println(fmt.Sprintf("%s%s", parentPrefix, field), bsonType)
+					continue
+				}
+
+				if subProperties, ok := value["properties"]; ok {
+					subProperties := subProperties.(bson.M)
+					qb.iterateProperties(
+						fmt.Sprintf("%s%s.", parentPrefix, field), subProperties)
+				}
 			}
+		default:
+			// unknown type
+			continue
 		}
 	}
-
-	return nil
 }
--- querybuilder_test.go
+++ querybuilder_test.go
@@ -1,19 +1,20 @@
 package mongo
 
 import (
+	"reflect"
 	"testing"
 
 	"go.mongodb.org/mongo-driver/bson"
 )
 
-func TestQueryBuilder_discoverFields(t *testing.T) {
+func TestNewQueryBuilder(t *testing.T) {
 	type args struct {
 		schema bson.M
 	}
 	tests := []struct {
-		name    string
-		args    args
-		wantErr bool
+		name string
+		args args
+		want map[string]string
 	}{
 		{
 			name: "test with basic schema",
@@ -53,31 +54,50 @@ func TestQueryBuilder_discoverFields(t *testing.T) {
 							"required": bson.A{},
 							"properties": bson.M{
 								"fieldA": bson.M{
-									"bsonType":    "",
-									"description": "",
+									"bsonType":    "array",
+									"description": "an array of elements",
 								},
 								"fieldB": bson.M{
-									"bsonType":    "",
-									"description": "",
+									"bsonType":    "date",
+									"description": "a nested date value",
 								},
 								"fieldC": bson.M{
-									"bsonType":   "object",
-									"required":   bson.A{},
-									"properties": bson.M{},
+									"bsonType": "object",
+									"required": bson.A{"fieldC-1"},
+									"properties": bson.M{
+										"fieldC-1": bson.M{
+											"bsonType":    "string",
+											"description": "nested two layers deep string",
+										},
+										"fieldC-2": bson.M{
+											"bsonType":    "double",
+											"description": "a double value",
+										},
+									},
 								},
 							},
 						},
 					},
 				},
 			},
-			wantErr: false,
+			want: map[string]string{
+				"someID":                         "string",
+				"created":                        "date",
+				"someName":                       "string",
+				"disabled":                       "bool",
+				"minMaxNumber":                   "int",
+				"childStructure.fieldB":          "date",
+				"childStructure.fieldC.fieldC-1": "string",
+				"childStructure.fieldC.fieldC-2": "double",
+				"childStructure.fieldA":          "array",
+			},
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			qb := QueryBuilder{}
-			if err := qb.discoverFields(tt.args.schema); (err != nil) != tt.wantErr {
-				t.Errorf("QueryBuilder.discoverFields() error = %v, wantErr %v", err, tt.wantErr)
+			qb := NewQueryBuilder(tt.args.schema)
+			if !reflect.DeepEqual(qb.fieldTypes, tt.want) {
+				t.Errorf("NewQueryBuilder(), qb.fieldTypes = %v, want %v", qb.fieldTypes, tt.want)
 			}
 		})
 	}
